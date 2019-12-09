f07efaee657416c6a8a08b0b35b6e39941d509a7
Upendra Sahu <usahu998@gmail.com>
2019-12-09 16:02:57 +0530

    Refactored: code to support list in place of Iterator

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -5,14 +5,15 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CSVBuilderException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            Iterator<IndiaCensusCSV> csvIterable = csvBuilder.getCSVFileIterator(reader, IndiaCensusCSV.class);
-            return getCount(csvIterable);
+            List<IndiaCensusCSV> csvIterable = csvBuilder.getCSVFileList(reader, IndiaCensusCSV.class);
+            return csvIterable.size();
         } catch (CSVBuilderException | IOException e) {
             throw new CSVBuilderException(e.getMessage(),
                     CSVBuilderException.ExceptionType.CENSUS_FILE_PROBLEM);
@@ -22,8 +23,8 @@ public class CensusAnalyser {
     public int loadIndianStateCode(String csvFilePath) throws CSVBuilderException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            Iterator<IndiaStateCodeCSV> csvIterable = csvBuilder.getCSVFileIterator(reader, IndiaStateCodeCSV.class);
-            return getCount(csvIterable);
+            List<IndiaStateCodeCSV> csvIterable = csvBuilder.getCSVFileList(reader, IndiaStateCodeCSV.class);
+            return csvIterable.size();
         } catch (CSVBuilderException | IOException e) {
             throw new CSVBuilderException(e.getMessage(),
                     CSVBuilderException.ExceptionType.CENSUS_FILE_PROBLEM);
--- src/main/java/censusanalyser/OpenCsvBuilder.java
+++ src/main/java/censusanalyser/OpenCsvBuilder.java
@@ -5,16 +5,25 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public class OpenCsvBuilder<E> implements ICSVBuilder {
+    @Override
     public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException {
+        return this.getCSVBean(reader, csvClass).iterator();
+    }
+
+    @Override
+    public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuilderException {
+        return this.getCSVBean(reader, csvClass).parse();
+    }
+
+    private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws CSVBuilderException {
         try {
             CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(csvClass);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            Iterator<E> censusCSVIterator = csvToBean.iterator();
-            return censusCSVIterator;
+            return csvToBeanBuilder.build();
         } catch (IllegalStateException e) {
             throw new CSVBuilderException(e.getMessage(), CSVBuilderException.ExceptionType.UNABLE_TO_PARSE);
         }
